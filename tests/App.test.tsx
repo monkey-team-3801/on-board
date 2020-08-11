8c5ec634536b07e46db1585885a2ca8e9d4adc4f
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-08-11 16:26:42 +1000

    Update jest paths and move tests

---
+++ tests/App.test.tsx
@@ -0,0 +1,7 @@
+// import React from "react";
+// import { render } from "@testing-library/react";
+// import { App } from "./App";
+
+test("Placeholder test", () => {
+    expect(1 + 1).toBe(2);
+});
