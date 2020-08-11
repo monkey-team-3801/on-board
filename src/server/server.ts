51adb372571c3b8ac4d48387888cd885b363b19e
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-08-11 22:02:10 +1000

    Setup simple server

---
+++ src/server/server.ts
@@ -0,0 +1,12 @@
+import express, { Express } from "express";
+import path from "path";
+
+const app: Express = express();
+
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "build/index.html"));
+});
+
+app.listen(5000);
