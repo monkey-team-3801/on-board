3135676402d64b29b3a072e789a1d4c26d23b093
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-08-11 22:32:03 +1000

    Formatting

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -332,24 +332,26 @@ module.exports = function (webpackEnv) {
 
                 // First, run the linter.
                 // It's important to do this before Babel processes the JS.
-                (isEnvDevelopment ? {
-                    test: /\.(js|mjs|jsx|ts|tsx)$/,
-                    enforce: "pre",
-                    use: [
-                        {
-                            options: {
-                                cache: true,
-                                formatter: require.resolve(
-                                    "react-dev-utils/eslintFormatter"
-                                ),
-                                eslintPath: require.resolve("eslint"),
-                                resolvePluginsRelativeTo: __dirname,
-                            },
-                            loader: require.resolve("eslint-loader"),
-                        },
-                    ],
-                    include: paths.appSrc,
-                } : {}),
+                isEnvDevelopment
+                    ? {
+                          test: /\.(js|mjs|jsx|ts|tsx)$/,
+                          enforce: "pre",
+                          use: [
+                              {
+                                  options: {
+                                      cache: true,
+                                      formatter: require.resolve(
+                                          "react-dev-utils/eslintFormatter"
+                                      ),
+                                      eslintPath: require.resolve("eslint"),
+                                      resolvePluginsRelativeTo: __dirname,
+                                  },
+                                  loader: require.resolve("eslint-loader"),
+                              },
+                          ],
+                          include: paths.appSrc,
+                      }
+                    : {},
                 {
                     // "oneOf" will traverse all following loaders until one will
                     // match the requirements. When no loader matches it will fall
