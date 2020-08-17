9d2f6233fbdea7f8ad6c92c6de3f6a38334d921c
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-08-18 09:10:02 +1000

    Revert "Revert "Setup MongoDB""

---
+++ src/server/database/database.ts
@@ -0,0 +1,31 @@
+import * as MongoDb from "mongodb";
+import * as Mongoose from "mongoose";
+import mongoose from "mongoose";
+
+export class Database {
+    private static connection: Mongoose.Connection;
+    private uri: string = "";
+
+    constructor(uri: string | undefined) {
+        this.uri = uri || "";
+    }
+
+    public async connect(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            mongoose.connect(this.uri, { useNewUrlParser: true });
+
+            const connection = mongoose.connection;
+            connection.on("error", (error) => {
+                reject(error);
+            });
+            connection.once("open", () => {
+                Database.connection = connection;
+                resolve();
+            });
+        });
+    }
+
+    public static getDb(): MongoDb.Db {
+        return this.connection.db;
+    }
+}
