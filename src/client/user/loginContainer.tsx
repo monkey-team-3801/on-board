11a92cee80a56098ee36b4f03e345b59477c3922
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-08-22 17:27:25 +1000

    Convert loginContainer to functional component

--- src/client/user/loginContainer.tsx
+++ src/client/user/loginContainer.tsx
@@ -1,63 +1,54 @@
 import React from "react";
 import { useDynamicFetch } from "../hooks";
-import { createUserRequestType, loginUserRequestType } from "../../types";
+import { CreateUserRequestType, LoginUserRequestType } from "../../types";
 
-export class loginContainer extends React.Component<{}, {}> {
-    constructor(props: {}) {
-        super(props);
+export const LoginContainer = () => {
+    const [username, setUsername] = React.useState<string>("");
+    const [password, setPassword] = React.useState<string>("");
 
-        this.state = {
-            username: "",
-            password: "",
-        };
-    }
+    const [data, loginUser] = useDynamicFetch<undefined, LoginUserRequestType>(
+        "user/login",
+        undefined,
+        false
+    );
 
-    handleSubmit(event: any) {
+    let handleSubmit = function (event: any) {
         event.preventDefault();
-        console.log(this.state);
+        //console.log(username, password);
 
-        const [data, loginUser] = useDynamicFetch<
-            undefined,
-            loginUserRequestType
-        >("user/login", undefined, false);
-    }
+        loginUser({ username: username, password: password });
+    };
 
-    render() {
-        return (
-            <div>
-                <h1>login test</h1>
-                <form onSubmit={(e) => this.handleSubmit(e)}>
-                    <label htmlFor="username">Enter Username</label>
-                    <br></br>
+    return (
+        <div>
+            <h1>login test</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label htmlFor="username">Enter Username</label>
+                <br></br>
 
-                    <input
-                        name="username"
-                        type="text"
-                        placeholder="username"
-                        onChange={(e) =>
-                            this.setState({ username: e.target.value })
-                        }
-                        required
-                    />
-                    <br></br>
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="username"
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <br></br>
 
-                    <label htmlFor="password">Enter Password</label>
-                    <br></br>
+                <label htmlFor="password">Enter Password</label>
+                <br></br>
 
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="password"
-                        onChange={(e) =>
-                            this.setState({ password: e.target.value })
-                        }
-                        required
-                    />
-                    <br></br>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <br></br>
 
-                    <button type="submit">login</button>
-                </form>
-            </div>
-        );
-    }
-}
+                <button type="submit">login</button>
+            </form>
+        </div>
+    );
+};
