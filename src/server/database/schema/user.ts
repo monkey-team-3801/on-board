f53e5b014af5b090652bc94443cc70f6d3b85286
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-08-23 12:38:08 +1000

    user schema update

--- src/server/database/schema/user.ts
+++ src/server/database/schema/user.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+interface IUser extends mongoose.Document {
+    username: string;
+    password: string;
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
     username: { type: String, trim: true, required: true },
     password: { type: String, required: true },
 });
 
-export const User = mongoose.model("User", UserSchema);
+export const User = mongoose.model<IUser>("User", UserSchema);
