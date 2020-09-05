39cad2281d4f267dd9f980c3ef8a54cd9629b8ce
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-09-06 02:20:36 +1000

    Added simple announcement job

---
+++ src/server/jobs/job-runner.ts
@@ -0,0 +1,26 @@
+import { io } from "../server";
+import { BaseJob, AnnouncementJob } from "../../types";
+import { isAnnouncementJob } from "../utils";
+import { Course } from "../database";
+import { User } from "../database/schema";
+import { AnnouncementEvent } from "../../events";
+
+const runAnnouncementJob = async (job: AnnouncementJob): Promise<void> => {
+    const user = await User.findById(job.data.user);
+    const course = await Course.findOne({ code: job.data.courseCode });
+    if (user) {
+        course?.announcements?.push({
+            ...job.data,
+            user: user.username,
+            date: job.jobDate,
+        });
+    }
+    await course?.save();
+    io.emit(AnnouncementEvent.NEW, job.data);
+};
+
+export const jobRunner = (job: BaseJob): void => {
+    if (isAnnouncementJob(job)) {
+        runAnnouncementJob(job);
+    }
+};
