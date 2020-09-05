bcb88297d2a983f47b24719405f502916c9e5bc9
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-09-06 02:24:08 +1000

    Update existing schemas to support jobs

--- src/server/database/schema/User.ts
+++ src/server/database/schema/User.ts
@@ -7,12 +7,14 @@ export interface IUser extends mongoose.Document {
     username: string;
     password: string;
     userType: UserType;
+    courses: Array<string>;
 }
 
 const UserSchema = new mongoose.Schema<IUser>({
     username: { type: String, trim: true, required: true, unique: true },
     password: { type: String, required: true },
     userType: { type: UserType, required: true },
+    courses: { type: Array, required: true }
 });
 
 export const User = mongoose.model<IUser>("User", UserSchema);
