393db98a9de567a7c3d4c84839d3c76c5cd2aedf
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-09-06 19:26:57 +1000

    Add some console debug lines

--- src/server/routes/job.ts
+++ src/server/routes/job.ts
@@ -9,6 +9,12 @@ export const router = express.Router();
 router.post(
     "/create",
     asyncHandler<{}, {}, BaseJob>(async (req, res) => {
+        console.log("Job scheduled", new Date(req.body.jobDate).toISOString());
+        console.log("Current time is", new Date().toISOString());
+        console.log(
+            "Executing in",
+            new Date(req.body.jobDate).getTime() - new Date().getTime()
+        );
         const schedulerHandler: ScheduleHandler = ScheduleHandler.getInstance();
         schedulerHandler.addNewJob({
             ...req.body,
