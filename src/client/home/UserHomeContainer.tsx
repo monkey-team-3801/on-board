8fc80c4f86d87ec158215c799d31883c9aae6116
Oscar Dunstan <oscardunstan2@gmail.com>
2020-09-07 19:35:54 +1000

    Styled announcements

--- src/client/home/UserHomeContainer.tsx
+++ src/client/home/UserHomeContainer.tsx
@@ -91,14 +91,94 @@ export const UserHomeContainer: React.FunctionComponent<Props> = (
                                     <Row>
                                         <h1>Announcements</h1>
                                     </Row>
+                                    <Row className="announcements">
+                                        <Container>
+                                            <Col>
+                                                <Row>
+                                                    <Col
+                                                        xl="1"
+                                                        xs
+                                                        lg="1"
+                                                        className="no-padding"
+                                                    >
+                                                        <div className="class-1-banner"></div>
+                                                    </Col>
+                                                    <Col xl="10" xs lg="10">
+                                                        <Row>
+                                                            <div>
+                                                                <h3>
+                                                                    CSSE1001
+                                                                </h3>
+                                                            </div>
+                                                        </Row>
+                                                        <Row>
+                                                            <div>
+                                                                <h4>
+                                                                    Assignment 1
+                                                                    Due
+                                                                </h4>
+                                                            </div>
+                                                        </Row>
+                                                        <Row>
+                                                            <div>
+                                                                <p className="announcement-time">
+                                                                    Today
+                                                                    10:30am
+                                                                </p>
+                                                            </div>
+                                                        </Row>
+                                                        <Row>
+                                                            <div>
+                                                                <p>
+                                                                    A reminder
+                                                                    your
+                                                                    assignment
+                                                                    is due
+                                                                    tomorrow.
+                                                                    Good luck!
+                                                                </p>
+                                                            </div>
+
+                                                            <Container>
+                                                                <Row>
+                                                                    <Col
+                                                                        xl="1"
+                                                                        xs
+                                                                        lg="1"
+                                                                    >
+                                                                        <div className="class-1-teacher"></div>
+                                                                    </Col>
+
+                                                                    <Col>
+                                                                        <div className="class-1-teacher-name">
+                                                                            <h5>
+                                                                                Richard
+                                                                                Thomas
+                                                                            </h5>
+                                                                        </div>
+                                                                    </Col>
+                                                                </Row>
+                                                            </Container>
+                                                            <div className="closing-banner"></div>
+                                                        </Row>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Container>
+                                    </Row>
                                 </Container>
                             </Col>
                         </Row>
                         <Container fluid>
                             <Row className="no-padding">
-                                <Col xs lg="6" xl="6" className="classes no-padding">
+                                <Col
+                                    xs
+                                    lg="6"
+                                    xl="6"
+                                    className="classes no-padding"
+                                >
                                     <h1>Upcoming Classes</h1>
-                                    <Row >
+                                    <Row>
                                         <Col className="class-1">
                                             <Container>
                                                 <div className="class-1-background">
@@ -124,7 +204,10 @@ export const UserHomeContainer: React.FunctionComponent<Props> = (
                                     <Row>
                                         <Col className="open-rooms">
                                             <RoomDisplayContainer
-                                                data={sessionResponse.data.sessions}
+                                                data={
+                                                    sessionResponse.data
+                                                        .sessions
+                                                }
                                                 onDeleteClick={onDeleteClick}
                                                 onJoinClick={onJoinClick}
                                             />
@@ -132,16 +215,16 @@ export const UserHomeContainer: React.FunctionComponent<Props> = (
                                     </Row>
                                     <Row>
                                         <CreateRoomPage
-                                            createRoom={async (name: string) => {
+                                            createRoom={async (
+                                                name: string
+                                            ) => {
                                                 await createRoom({ name });
                                                 await refresh();
                                             }}
                                         />
                                     </Row>
                                 </Col>
-
                             </Row>
-
                         </Container>
                     </Container>
                 </Col>
