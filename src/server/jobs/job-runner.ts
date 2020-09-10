d67cfc6c60ddeb1f0e644148462c071c4fb96e31
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-09-11 03:04:34 +1000

    Added job to announce class open

--- src/server/jobs/job-runner.ts
+++ src/server/jobs/job-runner.ts
@@ -1,9 +1,13 @@
 import { io } from "../server";
-import { BaseJob, AnnouncementJob } from "../../types";
-import { isAnnouncementJob } from "../utils";
+import { BaseJob, AnnouncementJob, ClassOpenJob } from "../../types";
+import {
+    isAnnouncementJob,
+    isClassOpenJob,
+    createNewClassroomSession,
+} from "../utils";
 import { Course } from "../database";
 import { User } from "../database/schema";
-import { AnnouncementEvent } from "../../events";
+import { AnnouncementEvent, ClassEvent } from "../../events";
 
 const runAnnouncementJob = async (job: AnnouncementJob): Promise<void> => {
     console.log("Running job", job.jobDate, job.data.user);
@@ -20,8 +24,26 @@ const runAnnouncementJob = async (job: AnnouncementJob): Promise<void> => {
     io.in(`${job.data.courseCode}_ANNOUNCEMENT`).emit(AnnouncementEvent.NEW);
 };
 
+const runClassOpenJob = async (job: ClassOpenJob): Promise<void> => {
+    console.log("Running class job", job);
+    const session = await createNewClassroomSession(
+        job.data.roomName,
+        job.data.description,
+        job.data.courseCode,
+        job.data.startTime,
+        job.data.endTime
+    );
+    io.in(`${job.data.courseCode}_ANNOUNCEMENT`).emit(ClassEvent.OPEN, {
+        id: session._id,
+        course: job.data.courseCode,
+        roomName: job.data.roomName,
+    });
+};
+
 export const jobRunner = (job: BaseJob): void => {
     if (isAnnouncementJob(job)) {
         runAnnouncementJob(job);
+    } else if (isClassOpenJob(job)) {
+        runClassOpenJob(job);
     }
 };
