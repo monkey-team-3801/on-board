ba4cfc9110377a218a213b3161ac7f532028b48b
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-09-11 03:05:32 +1000

    Added classroom session schema

--- src/server/database/schema/Session.ts
+++ src/server/database/schema/Session.ts
@@ -1,14 +1,44 @@
 import mongoose from "mongoose";
-import { MessageData } from "../../../types";
+import { MessageData, RoomType } from "../../../types";
 
 interface ISession extends mongoose.Document {
     name: string;
-    messages?: Array<Omit<MessageData, "sessionId">>;
+    messages: Array<Omit<MessageData, "sessionId">>;
+    description: string;
+    roomType: RoomType;
+    courseCode?: string;
+}
+
+// interface PrivateRoom extends ISession {
+//     roomType: RoomType.PRIVATE;
+// }
+
+interface IClassroomSession extends ISession {
+    roomType: RoomType.CLASS;
+    courseCode: string;
+    startTime: string;
+    endTime: string;
 }
 
 const SessionSchema = new mongoose.Schema<ISession>({
     name: { type: String, required: true },
     messages: { type: Array, default: [] },
+    roomType: { type: Number },
 });
 
 export const Session = mongoose.model<ISession>("Session", SessionSchema);
+
+const ClassroomSessionSchema = new mongoose.Schema<IClassroomSession>({
+    name: { type: String, required: true },
+    messages: { type: Array, default: [] },
+    roomType: { type: Number },
+    description: { type: String },
+    courseCode: { type: String, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+});
+
+export const ClassroomSession = mongoose.model<IClassroomSession>(
+    "ClassroomSession",
+    ClassroomSessionSchema
+);
