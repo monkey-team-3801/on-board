0b053787790da3d2fdf0e85a4585900767acf95f
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-09-13 00:34:10 +1000

    download functionality

--- src/client/filehandler/FileContainer.tsx
+++ src/client/filehandler/FileContainer.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDynamicFetch } from "../hooks";
+import "../styles/FileContainer.less";
+import { FaDownload } from "react-icons/fa";
+import { FileStorageType } from "../../types";
 
 type Props = {
     sessionId: string;
@@ -12,20 +15,46 @@ export const FileContainer: React.FunctionComponent<Props> = (props: Props) => {
         true
     );
 
+    function sizeDisplay(size: number): string {
+        if (size === 0) {
+            return "0 Bytes";
+        }
+
+        const sizes = ["Bytes", "KB", "MB"];
+        const k = 1024;
+        const i = Math.floor(Math.log(size) / Math.log(k));
+
+        return parseFloat((size / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+    }
+
+    function download(name: string): void {
+        let link = document.createElement("a");
+        let blob = new Blob([data.data[name].file], {
+            type: data.data[name].fileExtension,
+        });
+        link.href = window.URL.createObjectURL(blob);
+        link.download = data.data[name].filename;
+        link.click();
+    }
+
     return (
-        <div>
-            <h1>document test</h1>
+        <div className="file-container">
+            <h1 className="file-list-header">Uploaded Files</h1>
             <div>
                 {data.data &&
                     Object.keys(data.data).map((files, i) => (
-                        <div className="file-list-container" key={i}>
+                        <div className="file-bar" key={i}>
                             <div>
-                                <div className="file-logo"></div>
+                                <button
+                                    className="file-dl-btn"
+                                    onClick={() => download(files)}
+                                >
+                                    <FaDownload />
+                                </button>
+
                                 <div className="file-name">
-                                    {data.data[files].filename}
-                                </div>
-                                <div className="file-size">
-                                    {data.data[files].size}
+                                    {data.data[files].filename}{" "}
+                                    {sizeDisplay(data.data[files].size)}
                                 </div>
                             </div>
                         </div>
