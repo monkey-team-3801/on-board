92ae2a85a692058bbf0f5608283d95d07ff9e10d
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-09-13 01:22:37 +1000

    minor comments

--- src/client/filehandler/UploadTest.tsx
+++ src/client/filehandler/UploadTest.tsx
@@ -3,6 +3,8 @@ import { UploadContainer } from "./UploadContainer";
 import { FileUploadType } from "../../types";
 import { ProfilePictureContainer } from "../user/ProfilePictureContainer";
 
+// REMOVE THIS FILE WHEN TEMP/TESTING IS NO LONGER NEEDED.
+
 type Props = {
     userId: string;
 };
