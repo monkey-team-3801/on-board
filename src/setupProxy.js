ab7148b47df545946750b6c1aadd0822fbad36c9
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-09-13 17:48:30 +1000

    prettier fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,9 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(createProxyMiddleware("/filehandler/**", { target: "http://localhost:5000" }));
+    app.use(
+        createProxyMiddleware("/filehandler/**", {
+            target: "http://localhost:5000",
+        })
+    );
 };
