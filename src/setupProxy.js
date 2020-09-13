51a42e64cef46c9da5d152a8de726997c243f8c2
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-09-13 17:44:35 +1000

    eslint fixes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,4 +2,4 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(createProxyMiddleware("/filehandler/**", { target: "http://localhost:5000" }));
-}
+};
