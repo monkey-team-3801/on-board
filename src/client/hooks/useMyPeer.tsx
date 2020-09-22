0d517fffd1b6fac4b41855e79e8be3638ba9150c
Mike <mikepham1207@gmail.com>
2020-09-22 10:42:22 +1000

    Attempt to fix port configuration for peerjs

--- src/client/hooks/useMyPeer.tsx
+++ src/client/hooks/useMyPeer.tsx
@@ -6,12 +6,15 @@ export type Peers = Map<PeerId, Peer.MediaConnection>;
 
 export const useMyPeer: () => [Peer, PeerId] = () => {
     // TODO: change hard coded port
-    const options = {
+    const options: Peer.PeerJSOption = {
         host: window.location.hostname,
-        port: Number(window.location.port) || 5000,
+        //port: Number(window.location.port) || 5000,
         path: "/peerServer",
         secure: process.env.NODE_ENV === "production"
     };
+    if (process.env.NODE_ENV === "development") {
+        options.port = Number(process.env.PORT) || 5000;
+    }
     console.log(options);
     console.log("port:", window.location.port);
     const [myPeer, setMyPeer] = useState<Peer>(
