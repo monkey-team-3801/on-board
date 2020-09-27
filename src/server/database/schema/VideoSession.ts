519c8590e325a9dbc27c2d2b436b046b81081e98
Mike Pham <mike.pham@uq.edu.au>
2020-09-27 22:52:04 +1000

    Add screen sharing control fields to VideoSession schema

--- src/server/database/schema/VideoSession.ts
+++ src/server/database/schema/VideoSession.ts
@@ -4,12 +4,16 @@ interface IVideoSession extends mongoose.Document {
     sessionId: string;
     userPeerMap: Map<string, string>;
     userReferenceMap: Map<string, number>;
+    numScreensAllowed: number;
+    sharingUsers: Array<string>;
 }
 
 const VideoSessionSchema = new mongoose.Schema<IVideoSession>({
     sessionId: { type: String, required: true, unique: true },
     userPeerMap: { type: Map, default: new Map() },
     userReferenceMap: { type: Map, default: new Map() },
+    numScreensAllowed: { type: Number, default: 1 },
+    sharingUsers: { type: Array, default: [] }
 });
 
 export const VideoSession = mongoose.model<IVideoSession>(
