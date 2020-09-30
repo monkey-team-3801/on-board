c9e28df56bf45534bdba032dc23227bb16221abc
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-09-30 23:52:44 +1000

    Added room grouping to breakout room creation

--- src/client/rooms/containers/RoomGroupingContainer.tsx
+++ src/client/rooms/containers/RoomGroupingContainer.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { UserData } from "../types";
+import { Col, Container, Row } from "react-bootstrap";
+import { UserDisplay } from "../components";
 
 type Props = {
-    users: Array<string>;
+    roomIndex: number;
+    roomAmount: number;
+    users: Array<UserData>;
+    allocateUser: (
+        roomIndex: number,
+        currentRoomIndex: number,
+        userId: string
+    ) => void;
+    minColSize?: number;
 };
 
-export const RoomGroupingContainer = () => {
-    return <div></div>;
+export const RoomGroupingContainer: React.FunctionComponent<Props> = (
+    props: Props
+) => {
+    return (
+        <Row>
+            <h5>
+                {props.roomIndex === 0
+                    ? "Main Room"
+                    : `Room ${props.roomIndex}`}
+            </h5>
+            <Container className="room-group">
+                <Row>
+                    {props.users.map((user) => {
+                        return (
+                            <Col xs={props.minColSize ?? 2} key={user.id}>
+                                <UserDisplay
+                                    {...user}
+                                    currentRoomIndex={props.roomIndex}
+                                    roomAmount={props.roomAmount}
+                                    allocateUser={props.allocateUser}
+                                />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+        </Row>
+    );
 };
