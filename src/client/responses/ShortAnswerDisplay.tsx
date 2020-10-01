0d71ff8a77bb503aea95f7e940e72ab0e9018ecb
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-10-01 22:32:40 +1000

    Basic display for short answer questions

--- src/client/responses/ShortAnswerDisplay.tsx
+++ src/client/responses/ShortAnswerDisplay.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 type Props = {
     formID: string;
+    q: string;
+    uid: string;
 };
 
 export const ShortAnswerDisplay = (props: Props) => {
-    return <div>{props.formID}</div>;
+    const [userResponse, setUserResponse] = React.useState<string>("");
+
+    const submit = (event: React.FormEvent<HTMLElement>) => {
+        event.preventDefault();
+        console.log(userResponse);
+    };
+
+    return (
+        <div>
+            <h1>{props.q}</h1>
+            <Form
+                onSubmit={(e) => {
+                    submit(e);
+                }}
+            >
+                <Form.Label>Enter your response here:</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    onChange={(e) => {
+                        setUserResponse(e.target.value);
+                    }}
+                />
+                <br></br>
+                <Button type="submit">Submit</Button>
+            </Form>
+        </div>
+    );
 };
