0bb780fd8ec9c26f8d284f53e7447930084b9f30
Xinyi Li <leletonyli.13944@yahoo.com.au>
2020-10-03 23:23:45 +1000

    Setup participants container with filtering

---
+++ src/client/rooms/containers/ParticipantsContainer.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Col, Form } from "react-bootstrap";
+import { UserDataResponseType } from "../../../types";
+import { UserDisplay, Participants } from "../components";
+
+type Props = {
+    users: Array<Omit<UserDataResponseType, "courses">>;
+    raisedHandUsers: Array<string>;
+};
+
+export const ParticipantsContainer: React.FunctionComponent<Props> = (
+    props: Props
+) => {
+    const { users } = props;
+
+    const [filterValue, setFilterValue] = React.useState<string>("");
+
+    const filteredUsers = React.useMemo(() => {
+        return filterValue === ""
+            ? users
+            : users.filter((user) => {
+                  return user.username.includes(filterValue);
+              });
+    }, [filterValue, users]);
+
+    return (
+        <Container>
+            <Form.Control
+                type="text"
+                onChange={(e) => {
+                    setFilterValue(e.target.value);
+                }}
+            />
+            <Participants
+                users={filteredUsers}
+                raisedHandUsers={props.raisedHandUsers}
+            />
+        </Container>
+    );
+};
