c52e5352e3d57602ba40b5a65ecf566acb72a632
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-10-07 18:12:49 +1000

    Removed duplicate audio

--- src/client/responses/DisplayContainer.tsx
+++ src/client/responses/DisplayContainer.tsx
@@ -45,8 +45,6 @@ export const DisplayContainer = (props: Props) => {
 
     const updateForms = React.useCallback(() => {
         getForms({ sid: props.sessionID });
-        const notification = new Audio("/public/notification.wav");
-        notification.play();
     }, [getForms, props.sessionID]);
 
     React.useEffect(() => {
