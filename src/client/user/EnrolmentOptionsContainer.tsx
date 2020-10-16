7f998fc47fa723326d3cb8b71139d956581514ca
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-10-16 17:35:54 +1000

    Basic layout

---
+++ src/client/user/EnrolmentOptionsContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const EnrolmentOptionsContainer: React.FunctionComponent = () => {
+    return (
+        <div>
+            <h1>Enrolment Options</h1>
+        </div>
+    );
+};
--- src/client/user/ProfilePage.tsx
+++ src/client/user/ProfilePage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Col, Container, Nav, NavItem, Row } from "react-bootstrap";
+import { ProfileOptionsContainer } from "./ProfileOptionsContainer";
 import { ProfilePictureContainer } from "./ProfilePictureContainer";
 
 type Props = {
@@ -6,11 +8,56 @@ type Props = {
 };
 
 export const ProfilePage: React.FunctionComponent<Props> = (props: Props) => {
+    const [navState, setNavState] = React.useState<number>(0);
+
     return (
-        <div>
-            <ProfilePictureContainer
-                userId={props.userID}
-            ></ProfilePictureContainer>
-        </div>
+        <Container fluid>
+            <Row>
+                <h1>Settings</h1>
+            </Row>
+            <Row>
+                <Col>
+                    <Nav variant="pills" defaultActiveKey="general">
+                        <NavItem className="flex-column">
+                            <Nav.Link
+                                eventKey="general"
+                                active={navState === 0}
+                                onClick={() => {
+                                    setNavState(0);
+                                }}
+                            >
+                                General
+                            </Nav.Link>
+                            <Nav.Link
+                                active={navState === 1}
+                                onClick={() => {
+                                    setNavState(1);
+                                }}
+                            >
+                                Enrolment
+                            </Nav.Link>
+                            <Nav.Link
+                                active={navState === 2}
+                                onClick={() => {
+                                    setNavState(2);
+                                }}
+                            >
+                                Account
+                            </Nav.Link>
+                        </NavItem>
+                    </Nav>
+                </Col>
+                <Col>
+                    <Row>
+                        <Col>
+                            <ProfileOptionsContainer />
+                        </Col>
+                        <Col>
+                            <ProfilePictureContainer userId={props.userID} />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     );
 };
