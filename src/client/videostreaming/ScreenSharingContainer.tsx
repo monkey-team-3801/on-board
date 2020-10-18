5a9129ef8f767bd215078751ab35b69f6e1f118a
Mike <mikepham1207@gmail.com>
2020-10-18 13:28:00 +1000

    Prettier again :(

--- src/client/videostreaming/ScreenSharingContainer.tsx
+++ src/client/videostreaming/ScreenSharingContainer.tsx
@@ -35,9 +35,11 @@ export const ScreenSharingContainer: React.FunctionComponent<Prop> = (
             </Row>
             <Row>
                 <Col xs="auto">
-                    <Button onClick={async () => {
-                        await setupScreenSharing();
-                    }}>
+                    <Button
+                        onClick={async () => {
+                            await setupScreenSharing();
+                        }}
+                    >
                         Start Sharing
                     </Button>
                 </Col>
